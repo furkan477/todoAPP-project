5aafbaa3bc978eeaac1893d88860900e9469dca2
Furkan kaya <78097624+furkan477@users.noreply.github.com>
2023-04-12 21:17:35 +0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -4,4 +4,9 @@
     const URL = 'http://localhost/php-patikas%C4%B1/todoapp/';
     const DEV_MODE = true;
 
+    try{
+        $db = new PDO('mysql:host=localhost;dbname=todoapp;','root','');
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
 ?>
