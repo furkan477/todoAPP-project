e6b5310bfbc71007e908db4cb3896329e5368174
Furkan kaya <78097624+furkan477@users.noreply.github.com>
2023-04-12 21:19:07 +0300

    Update login.php

--- controller/login.php
+++ controller/login.php
@@ -1,16 +1,31 @@
 <?php
+    if(get_session('login') && get_session('login') == true) redirect('home');
 
     if (route(0) == 'login'){
 
         if(isset($_POST['submit'])){
+            $_SESSION['post'] = $_POST;
             add_session('hata','mesajınız eklendi!');
-            $eposta =post('eposta');
+            $eposta = post('eposta');
             $sifre = post('sifre');
 
-            echo $eposta.' '.$sifre;
+            $return = model('auth/login',['email' => $eposta, 'passwords' => $sifre],'login');
+
+            if($return['success'] == true){
+                if (isset($return['redirect'])){
+                    redirect($return['redirect']);
+                }
+            }else{
+                add_session('error',[
+                    'message' => $return['message'] ?? '',
+                    'type' => $return['type'] ?? ''
+                ]);
+            }
 
         }
+        
         view('auth/login');
     }
+
 
 ?>
