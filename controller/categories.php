2932d25c5ac05a6a5f31b0a96844dd3d1a121dd6
Furkan kaya <78097624+furkan477@users.noreply.github.com>
2023-04-25 16:18:52 +0300

    Update categories.php

--- controller/categories.php
+++ controller/categories.php
@@ -1,75 +1,99 @@
 <?php
-    if(!get_session('login') || get_session('login') != true) redirect('login');
 
-    if (route(0) == 'categories' && !route(1)){
-        
-        view('categories/home');
-    }elseif(route(0) == 'categories' && route(1) == 'add'){
+if (!get_session('login') || get_session('login') != true) redirect('login');
 
-        if(isset($_POST['submit'])){
-            $_SESSION['title'] = $_POST;
-            $title = post('title');
+if (route(0) == 'categories' && !route(1)){
 
-            $return = model('categories',['title' => $title],'add');
-            
+    /*if (isset($_POST['submit'])){
+        $_SESSION['post'] = $_POST;
+        $eposta = post('eposta');
+        $sifre = post('sifre');
 
-            if($return['success'] == true){
-                if (isset($return['redirect'])){
-                    redirect($return['redirect']);
-                }
-            }else{
-                add_session('error',[
-                    'message' => $return['message'] ?? '',
-                    'type' => $return['type'] ?? ''
-                ]);
+        $return = model('auth/login',['email' => $eposta, 'password' => $sifre], 'login');
+
+        if ($return['success'] == true){
+            if (isset($return['redirect'])){
+                redirect($return['redirect']);
             }
+        }else{
+            add_session('error',[
+                'message' => $return['message'] ?? '',
+                'type' => $return['type'] ?? ''
+            ]);
 
         }
+    }*/
 
-        view('categories/add');
-    }elseif(route(0) == 'categories' && route(1) == 'list'){
+    view('categories/home');
+}
 
-        $return = model('categories',[],'list');
+elseif (route(0) == 'categories' && route(1) == 'add'){
 
-        view('categories/list',$return['data']);
+    if (isset($_POST['submit'])){
+        $_SESSION['post'] = $_POST;
+        $title = post('title');
 
-    }elseif(route(0) == 'categories' && route(1) == 'edit' && is_numeric(route(2))){
+        $return = model('categories',['title' => $title], 'add');
 
-        if(isset($_POST['submit'])){    
-            $_SESSION['title'] = $_POST;
-            $title = post('title');
-            $id = post('id');
-
-            $return = model('categories',['title' => $title,'id' => $id],'edit');
-            
-
-            if($return['success'] == true){
-                add_session('error',[
-                    'message' => $return['message'] ?? '',
-                    'type' => $return['type'] ?? ''
-                ]);
-                if (isset($return['redirect'])){
-                    redirect($return['redirect']);
-                }
-            }else{
-                add_session('error',[
-                    'message' => $return['message'] ?? '',
-                    'type' => $return['type'] ?? ''
-                ]);
+        if ($return['success'] == true){
+            if (isset($return['redirect'])){
+                redirect($return['redirect']);
             }
+        }else{
+            add_session('error',[
+                'message' => $return['message'] ?? '',
+                'type' => $return['type'] ?? ''
+            ]);
 
         }
-
-        $return = model('categories',['id' => route(2)],'getsingle');
-
-        view('categories/edit',$return['data']);
-    }elseif(route(0) == 'categories' && route(1) == 'remove' && is_numeric(route(2))){
-        
-        $return = model('categories',['id' => route(2)],'remove');
-
-        redirect('tr/categories/list');
-
     }
 
 
-?>
+    view('categories/add');
+}
+
+elseif (route(0) == 'categories' && route(1) == 'list'){
+
+    $return = model('categories', [], 'list');
+
+    view('categories/list', $return['data']);
+}
+
+elseif (route(0) == 'categories' && route(1) == 'edit' && is_numeric(route(2))){
+
+    if (isset($_POST['submit'])){
+        $_SESSION['post'] = $_POST;
+        $title = post('title');
+        $id = post('id');
+
+        $return = model('categories',['title' => $title, 'id' => $id], 'edit');
+
+        if ($return['success'] == true){
+            add_session('error',[
+                'message' => $return['message'] ?? '',
+                'type' => $return['type'] ?? ''
+            ]);
+            if (isset($return['redirect'])){
+                redirect($return['redirect']);
+            }
+        }else{
+            add_session('error',[
+                'message' => $return['message'] ?? '',
+                'type' => $return['type'] ?? ''
+            ]);
+
+        }
+    }
+
+    $return = model('categories', ['id' => route(2)], 'getsingle');
+
+    view('categories/edit', $return['data']);
+}
+
+elseif (route(0) == 'categories' && route(1) == 'remove' && is_numeric(route(2))){
+
+    $return = model('categories', ['id' => route(2)], 'remove');
+
+    redirect('categories/list/?type='.$return['type'].'&message='.$return['message']);
+
+}
