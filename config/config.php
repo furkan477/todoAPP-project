6f1528008b15d448f4a030c85aa4387879912531
Furkan kaya <78097624+furkan477@users.noreply.github.com>
2023-04-25 16:11:36 +0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,12 +1,11 @@
 <?php
 
-    const BASEDIR = 'C:\xampp\htdocs\php-patikası\todoapp';
-    const URL = 'http://localhost/php-patikas%C4%B1/todoapp/';
-    const DEV_MODE = true;
+const BASEDIR = 'C:\xampp\htdocs\php-patikası\todoapp';
+const URL = 'http://localhost/todoapp/';
+const DEV_MODE = true;
 
-    try{
-        $db = new PDO('mysql:host=localhost;dbname=todoapp;','root','');
-    }catch(PDOException $e){
-        echo $e->getMessage();
-    }
-?>
+try {
+    $db = new PDO('mysql:host=localhost;dbname=todoapp;','root','');
+}catch (PDOException $e){
+    echo $e->getMessage();
+}
